7859a9755a8053651156b5eaddf90826cd5fd9a5
NamPT <montossc@gmail.com>
2026-03-16 13:37:33 +0700

    feat(bd-1zx.3): add barrel export for UI primitives
    - Exports Button, Input, Badge, Panel from src/lib/components/ui/index.ts

---
+++ src/lib/components/ui/index.ts
@@ -0,0 +1,4 @@
+export { default as Badge } from "./Badge.svelte";
+export { default as Button } from "./Button.svelte";
+export { default as Input } from "./Input.svelte";
+export { default as Panel } from "./Panel.svelte";
